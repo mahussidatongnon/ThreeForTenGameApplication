7ea1544b394b740712fc736b743dbe8a3170191d
Mahussi DATONGNON <mahussidatongnon@gmail.com>
2025-02-20 22:13:02 +0100

    setup HelloWorld

--- src/main/kotlin/com/miageia2/ThreeForTengame/controllers/HelloWorld.kt
+++ src/main/kotlin/com/miageia2/ThreeForTengame/controllers/HelloWorld.kt
@@ -1,16 +1,24 @@
 package com.miageia2.ThreeForTengame.controllers
 
-import org.springframework.http.HttpStatus
+import com.fasterxml.jackson.databind.json.JsonMapper
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController
-@RequestMapping("/hello-world")
+
+@RestController(value = "")
+@RequestMapping("/")
 class HelloWorld {
-    @GetMapping("/")
-    fun hello(): ResponseEntity<String> {
-        return ResponseEntity("Hello, World", HttpStatus.OK)
+    @GetMapping("/", produces = arrayOf(MediaType.APPLICATION_JSON_VALUE))
+    fun hello(): ResponseEntity<Any> {
+//        return ResponseEntity("Hello, World", HttpStatus.OK)
+        val mapper = JsonMapper()
+        val message = mapper.writeValueAsString(object : Any() {
+            @Suppress("unused")
+            val message: String = "Hello, World!"
+        })
+        return ResponseEntity.ok(message)
     }
 }
