d335639c8576d087fd8c62b05e3b463fd0c7197b
Mahussi DATONGNON <mahussidatongnon@gmail.com>
2025-04-27 20:23:37 +0200

    update play system

--- aiplayer/src/main/kotlin/com/miageia2/threefortengame/aiplayer/service/GameService.kt
+++ aiplayer/src/main/kotlin/com/miageia2/threefortengame/aiplayer/service/GameService.kt
@@ -3,11 +3,13 @@ package com.miageia2.threefortengame.aiplayer.service
 import com.miageia2.threefortengame.aiplayer.proxy.GameClient
 import com.miageia2.threefortengame.aiplayer.service.agent.QLearningAgent
 import com.miageia2.threefortengame.aiplayer.utils.SubscriptionsPlayersManager
+import com.miageia2.threefortengame.common.AiPlayerType
 import com.miageia2.threefortengame.common.dto.core.GamePartDTO
 import com.miageia2.threefortengame.common.dto.core.GameStateDTO
 import com.miageia2.threefortengame.common.dto.core.PlayGameDTO
 import com.miageia2.threefortengame.common.dto.core.PointDTO
 import org.springframework.stereotype.Service
+import com.miageia2.threefortengame.aiplayer.service.agent.Action
 
 @Service
 class GameService(private val gameClient: GameClient) {
@@ -15,13 +17,29 @@ class GameService(private val gameClient: GameClient) {
         return gameClient.getById(gameId)
     }
 
-    fun play(gameStateDTO: GameStateDTO) {
+    fun play(gameStateDTO: GameStateDTO, aiPlayerType: AiPlayerType = AiPlayerType.RANDOM_AI) {
         println("Playing ${gameStateDTO.id}")
         val subscriptionPlayerInfo = SubscriptionsPlayersManager.getItem(gameID = gameStateDTO.gamePartId!!,
             playerId = gameStateDTO.currentPlayerId!!)
-        val agent = QLearningAgent(startState = gameStateDTO.boardState!!)
-        agent.loadQValues(filePath = "qValues/${agent.startState.size}SquareCases/Best.json")
-        val action = agent.getPolicy(gameStateDTO.boardState!!)
+        val action: Action?
+
+        when (aiPlayerType) {
+            AiPlayerType.RANDOM_AI -> {
+                val legalActions: Array<PointDTO> = gameClient.getLegalActions(gameStateDTO.gamePartId!!)
+                val coordinates = legalActions[0]
+                action = Action(coordinates,3)
+            }
+            AiPlayerType.PASSIF_MOST_AWAY_CONNER_AI -> {
+                val legalActions: Array<PointDTO> = gameClient.getLegalActions(gameStateDTO.gamePartId!!)
+                val coordinates = legalActions[0]
+                action = Action(coordinates,3)
+            }
+            AiPlayerType.ACTIF_AI -> {
+                val agent = QLearningAgent(startState = gameStateDTO.boardState!!)
+                agent.loadQValues(filePath = "qValues/${agent.startState.size}SquareCases/Best.json")
+                action = agent.getPolicy(gameStateDTO.boardState!!)
+            }
+        }
 //        val legalActions: Array<PointDTO> = gameClient.getLegalActions(gameStateDTO.gamePartId!!)
         if (action != null) {
             if (subscriptionPlayerInfo != null) {
